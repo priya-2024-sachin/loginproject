3cfb11d76bc9191cd256012a2afb92d3f55424fd
priya-2024-sachin <priyadharshinig1987@gmail.com>
2024-09-25 17:19:56 +0530

    Add files via upload

---
+++ New app for login/models/index.js
@@ -0,0 +1,35 @@
+const config = require("../config/db.config.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+        host: config.HOST,
+        dialect: config.dialect,
+        pool:{
+            max: config.pool.max,
+            min: config.pool.min,
+            acquire: config.pool.acquire,
+            idle: config.pool.idle
+        }
+    }
+);
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
+
+db.user.belongsToMany(db.user,{
+    through:"user - roles"
+});
+db.role.belongsToMany(db.role,{
+    through:"user - roles"
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+module.exports = db;
